11f7188b2b0b8ddd8daaea8f80024741b980d8b0
tobzy <onuegbu.tobechukwu@yahoo.com>
2019-12-26 11:51:42 +0100

    Fixed side menu

--- src/lib/utils/registerPushNotifications.js
+++ src/lib/utils/registerPushNotifications.js
@@ -1,42 +1,60 @@
+import { Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
-import { apiRequest } from "../api/api";
 
-const PUSH_ENDPOINT = 'https://quickcredit.com.ng/test/api/me/details/pushtoken';
+const PUSH_ENDPOINT = 'https://test_notifications_api.herokuapp.com/api/users';
 
-async function registerForPushNotificationsAsync(username, token) {
-    // const {status: existingStatus} = await Permissions.getAsync(
-    //     Permissions.NOTIFICATIONS
-    // );
-    //
-    // let finalStatus = existingStatus;
-    //
-    // // only ask if permissions have not already been determined, because
-    // // iOS won't necessarily prompt the user a second time.
-    // if (existingStatus !== 'granted') {
-    //     // Android remote notification permissions are granted during the app
-    //     // install, so this will only ask on iOS
-    //     const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    //     finalStatus = status;
-    // }
-    //
-    // // Stop here if the user did not grant permissions
-    // if (finalStatus !== 'granted') {
-    //     return;
-    // }
+// async function registerForPushNotificationsAsync(email = '', updateUser, pushTokenSaved) {
+async function registerForPushNotificationsAsync(email = '', pushTokenSaved) {
+    console.log(pushTokenSaved);
+    if(pushTokenSaved){
+        return
+    }
+    const {status: existingStatus} = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
 
+
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+        // Android remote notification permissions are granted during the app
+        // install, so this will only ask on iOS
+        const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+        return;
+    }
+
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token)
 
     // POST the token to your backend server from where you can retrieve it to send push notifications.
+    fetch(PUSH_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token,
+            email,
+        }),
+    }).then(function (response) {
+        return response.json();
+    }).then(function (myJson) {
+        // updateUser({
+        //     pushTokenSaved:true
+        // })
+        console.log(JSON.stringify(myJson));
+    }).catch(error => {
+        console.log(error)
+    });
 
-    apiRequest(PUSH_ENDPOINT, 'patch', {
-        "username": username,
-        "push_token": token
-    }).then(res => {
-        console.log(res)
-    })
-        .catch(err => {
-            console.log(err)
-
-        });
 }
 
-export  {registerForPushNotificationsAsync};
+export { registerForPushNotificationsAsync }
